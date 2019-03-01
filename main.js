87631289498391edb39fec03d25b3b9e5ab06771
h22121 <h22121@lab.cpcc.edu>
2019-02-28 20:34:08 -0500

    Commit

---
+++ main.js
@@ -0,0 +1,6 @@
+//this is going to load the jquery ready function so that we wait for the page to load before the js
+$(document).ready(function(){
+    console.log('you are reading this from the console');
+});
+/*the dollar sign is a variable, created by jquery - it has one parameter - document - which has a ready function,
+so when it's ready, it'll excecute the other function.*/
