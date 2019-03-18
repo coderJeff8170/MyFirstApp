e69a1929ba189fd37c291fd9bdc0b8f00b0ae184
Jeff Andrews <jeff@paintedbyjeff.com>
2019-03-18 14:20:13 -0400

    contagious 1419 031819

--- MSJavaScriptLearningFolder/scopeBasics.js
+++ MSJavaScriptLearningFolder/scopeBasics.js
@@ -1,5 +1,5 @@
 let a = 'first';
-
+/*
 function scopeTest() {
     console.log(a);
     a = 'changed';
@@ -11,10 +11,21 @@ function scopeTest() {
         let c = 'third';
     }
 }
-
 scopeTest();
+*/
+
 /*console.log(b);*/
 
 // as above, the scope of a is available to the whole program. b is available only to the function, where it was born
 // a variable will be available to all child functions of the function in which it was created.
 // it will throw an undefined to anything above it.
+var sum = 0;
+for(i=4; i<8; i++) {
+    if(i == 6) {
+        continue;
+    }
+
+    sum += i;
+    console.log(sum);
+}
+console.log(sum);
