b2951102b942a098bc170f5132ce7f4e77e028f6
Jeff Andrews <jeff@paintedbyjeff.com>
2019-03-19 08:32:23 -0400

    home 0832 031919

--- MSJavaScriptLearningFolder/app.js
+++
@@ -1,6 +0,0 @@
-// console.log('Jeff is Awesome');
-
-let x = 7;
-let y = 3;
-let z = x + y;
-console.log('Answer: ' + z);
--- MSJavaScriptLearningFolder/datatypes.js
+++
@@ -1,8 +0,0 @@
-let x = true;
-console.log(typeof x);
-
-let y = 7;
-console.log(typeof y);
-
-let z = 'jeff is awesome';
-console.log(typeof z);
